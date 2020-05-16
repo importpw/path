c86a03863e70a220640fd1fb0a533e81a87759b6
Nathan Rajlich <nathan@tootallnate.net>
2020-05-15 22:45:51 -0700

    Add another `path_normalize` test and a failing one

--- path_test.sh
+++ path_test.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -eu
-eval "`curl -sfLS import.pw`"
+eval "$(curl -sfLS https://import.pw)"
 import "assert@2.1.3"
-source ./path.sh
+source "./path.sh"
 
 # path_normalize
+assert_equal "$(path_normalize "/one/.")" "/one"
 assert_equal "$(path_normalize "/foo/./bar/..")" "/foo"
+#assert_equal "$(path_normalize "/one/.///two")" "/one/two"
 
 # path_pretty
 assert_equal "$(HOME="/home/me" path_pretty "/home/me/foo.txt")" "~/foo.txt"
