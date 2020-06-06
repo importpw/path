9e0cd58003b7b1929676745110454bc511666123
Nathan Rajlich <nathan@tootallnate.net>
2020-06-05 19:50:50 -0700

    Add `.editorconfig` file

--- path.sh
+++ path.sh
@@ -9,17 +9,17 @@ import "string@0.1.0"
 #
 # Based on: https://stackoverflow.com/a/31086901/376773
 path_normalize() {(
-  shopt -s extglob
-  local path="$1"
+	shopt -s extglob
+	local path="$1"
 
-  # get rid of /../ example: /one/../two to /two
-  path="${path//\/*([!\/])\/\.\./}"
+	# get rid of /../ example: /one/../two to /two
+	path="${path//\/*([!\/])\/\.\./}"
 
-  # get rid of /./ and //* example: /one/.///two to /one/two
-  path="${path//@(\/\.\/|\/+(\/))//}"
+	# get rid of /./ and //* example: /one/.///two to /one/two
+	path="${path//@(\/\.\/|\/+(\/))//}"
 
-  # remove the last '/.'
-  echo "${path%%/.}"
+	# remove the last '/.'
+	echo "${path%%/.}"
 )}
 
 # Replaces a file path contained within the `$HOME` directory with `~`.
@@ -29,9 +29,9 @@ path_normalize() {(
 # "~/example.txt"
 # ```
 path_pretty() {
-  if string_starts_with "$1" "$HOME"; then
-    echo "~${1:${#HOME}}"
-  else
-    echo "$1"
-  fi
+	if string_starts_with "$1" "$HOME"; then
+		echo "~${1:${#HOME}}"
+	else
+		echo "$1"
+	fi
 }
